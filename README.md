5126f0fd4a9bc0acb42518ce81985cace830b4cf
l619534951 <619534951@qq.com>
2020-11-30 19:00:00 +0800

    init

--- README.md
+++ README.md
@@ -1,19 +1,15 @@
-# ant-design-vue-web-types
+# （临时解决方案）生成ant-design-vue的web-types.json文件，用于代码提示，不让代码检查出现警告
 
 ## Project setup
 ```
-npm install
+1、npm install
+2、npm update
+3、npm run serve
+4、打开浏览器，复制生成的json内容
+5、在需要代码提示的项目的node_modules/ant-design-vue/dist目录下新建web-types.json
+6、粘贴第4步的内容到web-types.json文件中
+7、node_modules/ant-design-vue/package.json文件新增一项"web-types": "dist/web-types.json"
+8、重新打开项目，有代码提示了
+注意不要使用cnpm，会启动不起来
 ```
 
-### Compiles and hot-reloads for development
-```
-npm run serve
-```
-
-### Compiles and minifies for production
-```
-npm run build
-```
-
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,68 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-</template>
+<script type="text/jsx">
+import Antd from 'ant-design-vue'
+import { version } from 'ant-design-vue'
 
-<script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+export default {
+  components: {  },
 
-export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-});
-</script>
+  setup() {
+    const components = []
+    const fakeApp = {
+      component: function (name, component) {
+        components.push(component)
+      },
+      use: function (component) {
+        component.install(fakeApp)
+      },
+      config: {
+        globalProperties: {}
+      },
+    }
+    Antd.install(fakeApp)
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    const createTag = component => {
+      // 只包含属性名称
+      const attributes = []
+      for (let key in component.props) {
+        attributes.push({
+          name: key
+        })
+      }
+
+      return {
+        name: component.name,
+        attributes: attributes
+      }
+    }
+
+    const tags = components.map(createTag)
+
+    const webTypes = {
+      $schema: 'http://json.schemastore.org/web-types',
+      framework: 'vue',
+      name: 'ant-design-vue',
+      version: version,
+      contributions: {
+        html: {
+          'types-syntax': 'typescript',
+          'description-markup': 'markdown',
+          tags,
+        },
+      },
+    }
+
+    const prettyResult = JSON.stringify(webTypes, null, 2)
+
+    return () => (
+        <div id="app">
+          <div class="json">{ prettyResult }</div>
+        </div>
+    )
+  },
 }
+</script>
+<style>
+  .json {
+    white-space: pre;
+  }
 </style>
